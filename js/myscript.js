b48a4fcabe930276b0d387913c9e49eb2a15d8a1
adamdrsinc <adam.sinc124@gmail.com>
2025-07-20 22:11:06 -0600

    Fully functional. Need to add reset.

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,34 @@
+window.addEventListener('blur', () => {
+    mouseDown = false;
+});
+
+
+let mouseDown = false;
+document.addEventListener('mousedown', () => {
+    mouseDown = true;
+});
+document.addEventListener('mouseup', () => {
+    mouseDown = false;
+});
+
 document.querySelector('[name="submitButton"]').addEventListener('click', generateGrid);
 
+let storedBackgroundColor = "";
 
+function trySketch(e){
+    if(mouseDown){
+        const target = e.target;
+        let color = target.style.backgroundColor;
+        const splitColors = color.split(',');
+        let leftVal = splitColors[0];
+
+        const colorVal = Number.parseInt(leftVal.substring(4));
+        const newColorVal = colorVal - 50 >= 0 ? colorVal - 50 : 0;
+
+        let newColor = `rgb(${newColorVal}, ${newColorVal}, ${newColorVal}`;
+        target.style.backgroundColor = newColor;
+    }
+}
 
 function generateGrid(){
     const inputWidth = Number.parseInt(document.querySelector('[name="widthInput"]').value);
@@ -30,7 +58,27 @@ function addGridSquare(gridWidth, gridHeight){
 
     newSquare.style.width = gridWidth + "px";
     newSquare.style.height = gridHeight + "px";
+    newSquare.style.backgroundColor = "rgb(255, 255, 255)";
     newSquare.classList.add("square");
+
+    newSquare.addEventListener('mouseenter', (e) => trySketch(e));
+    newSquare.addEventListener('mouseenter', (e) => onSquareHoverEnter(e));
+    newSquare.addEventListener('mouseleave', (e) => onSquareHoverExit(e));
 
     grid.appendChild(newSquare);
 }
+
+// function onSquareHoverEnter(e){
+//     if(!mouseDown){
+//         const hoverColor = "red";
+
+//         const target = e.target;
+//         storedBackgroundColor = target.style.backgroundColor;
+//         target.style.backgroundColor = hoverColor;
+//     }
+// }
+
+// function onSquareHoverExit(e){
+//         const target = e.target;
+//         target.style.backgroundColor = storedBackgroundColor;
+// }
